1f4859993b9104ef7ae50a180106c041dbce394d
Abdelrahman-Aldesoky <abdelrahman.mohamed.aldesoky@gmail.com>
2023-11-12 07:31:08 +0200

    General Improvements: Added library source files directory for improved configurability. Cleaning up all comments and tidying up. Added the correct examples for usage.

--- snake_effect/config.mk
+++ snake_effect/config.mk
@@ -5,8 +5,14 @@
 PROJECT_NAME := $(notdir $(CURDIR))
 
 # Micro controller Variables
-MCU = atmega32
-F_CPU = 8000000
+MCU ?= atmega32
+F_CPU ?= 8000000
+
+# Compiler flags linker flag
+CFLAGS ?= -mmcu=$(MCU) -DF_CPU=$(F_CPU) -std=gnu99 -O2 -Wall
+
+# Lib Makefile Directory
+LIB_MAKE_FILE := ./lib 
 
 # Main Makefile Directories Variables
 M_BUILD_DIR := ./build
@@ -14,20 +20,16 @@ M_DEPS_DIR := ./build/dependencies
 M_LIBS_DIR := ./build/libraries
 M_OBJS_DIR := ./build/objects
 M_SRCS_DIR := ./src
-M_INCS_DIR := ./inc
 
 # Lib Makefile Directories Variables
 L_BUILD_DIR := ../build
 L_DEPS_DIR := ../build/dependencies
 L_LIBS_DIR := ../build/libraries
 L_OBJS_DIR := ../build/objects
+L_SRCS_DIR := .
 
 # Toolchain definitions
 CC := avr-gcc
 AR := avr-ar
 OBJCOPY := avr-objcopy
 SIZE := avr-size
-
-# Compiler flags linker flag
-CFLAGS = -mmcu=$(MCU) -DF_CPU=$(F_CPU) -std=gnu99 -O2 -Wall
-LDFLAGS = -L$(M_LIBS_DIR)
